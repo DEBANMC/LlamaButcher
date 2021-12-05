858c84041339532e72cb18a46da464eb7ef84724
Ermolay <60964356+Egor-Ermolaev@users.noreply.github.com>
2021-12-05 19:11:12 +0300

    Update LlamaButcher.java

--- src/main/java/net/lldv/llamabutcher/LlamaButcher.java
+++ src/main/java/net/lldv/llamabutcher/LlamaButcher.java
@@ -97,7 +97,7 @@ public class LlamaButcher extends PluginBase {
                 else if (entity instanceof EntityPainting) kill = false;
                 else if (entity instanceof EntityEndCrystal) kill = false;
                 else if (entity instanceof EntityBoat) kill = false;
-                else if (entity instanceof EntityWither) kill = false;
+                else if (entity.getNetworkId() == 52) kill = false;
                 else if (entity instanceof EntityVehicle) kill = false; // minecarts
                 else if (entity.getNetworkId() == 61) kill = false; // armor stand FloatingTextParticle
 
